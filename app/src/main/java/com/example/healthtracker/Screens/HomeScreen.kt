be6df3714f01fabfc261eb2a0eeb1cfa5db3eb96
bberkedemir <164383460+bberkedemir@users.noreply.github.com>
2024-06-25 14:22:38 +0300

    Update HomeScreen.kt
    Reorganized boxes to fit screen. Also now uses data from ExerciseRepository.kt

--- app/src/main/java/com/example/healthtracker/Screens/HomeScreen.kt
+++ app/src/main/java/com/example/healthtracker/Screens/HomeScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -21,6 +22,9 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -38,11 +42,23 @@ import com.example.healthtracker.ui.theme.RoyalPurple
 @Composable
 fun HomeScreen(navController: NavHostController) {
 
+    val selectedDate by remember { mutableStateOf(ExerciseRepository.getDate(0)) }
+    val caloriesInfo = ExerciseRepository.getCaloriesForDate(selectedDate)
+    val kilometersRun = ExerciseRepository.getKmForDate(selectedDate, "run")
+    val minutesRun = ExerciseRepository.getMinForDate(selectedDate, "run")
+    val kilometersCycle = ExerciseRepository.getKmForDate(selectedDate, "cycle")
+    val minutesCycle = ExerciseRepository.getMinForDate(selectedDate, "cycle")
+    val kilometersHike = ExerciseRepository.getKmForDate(selectedDate, "hike")
+    val minutesHike = ExerciseRepository.getMinForDate(selectedDate, "hike")
+    val minutesTotal = ExerciseRepository.getTotalMinForDate(selectedDate)
+    val kilometersTotal = ExerciseRepository.getTotalKmForDate(selectedDate)
+
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(RoyalPurple)
-            .padding(50.dp),
+            .padding(20.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Row(
@@ -86,48 +102,52 @@ fun HomeScreen(navController: NavHostController) {
         Box(
             modifier = Modifier
                 .fillMaxWidth()
+                .height(76.dp)
                 .border(2.dp, Color.White.copy(alpha = 0.3f), shape = RoundedCornerShape(48.dp))
-                .height(100.dp)
                 .clickable {
                     // navigate exercise
                 }
-
-        ) {
+        )
+        {
             Row(
                 modifier = Modifier
-                    .padding(20.dp)
+                    .padding(16.dp)
                     .fillMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically
             ) {
+
                 Image(
                     painter = painterResource(id = R.drawable.running),
                     contentDescription = "running",
                     modifier = Modifier.clip(CircleShape)
                 )
-                Spacer(modifier = Modifier.width(15.dp))
 
-                Column() {
-                    Text(
-                        text = "Running",
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.White
-                    )
-                    Spacer(modifier = Modifier.height(5.dp))
-                    Text(text = "24 min", fontSize = 18.sp, color = Color.White)
+                Spacer(modifier = Modifier.width(12.dp))
+
+                Column()
+                {
+                    Text(text = "Running", fontSize = 18.sp, color = Color.White, fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = "$minutesRun min", fontSize = 14.sp, color = Color.White, fontWeight = FontWeight.Bold)
                 }
 
-                Spacer(modifier = Modifier.width(50.dp))
+                Spacer(modifier = Modifier.width(56.dp))
 
-                Column() {
-                    Text(
-                        text = "5.56 km",
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.White
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalAlignment = Alignment.End
+                )
+                {
+                    Text(text = "$kilometersRun km", fontSize = 18.sp, color = Color.White, fontWeight = FontWeight.Bold)
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Row(
+                        modifier = Modifier,
+                        verticalAlignment = Alignment.CenterVertically
                     )
-                    Spacer(modifier = Modifier.height(5.dp))
-                    Row {
+                    {
                         Image(
                             painter = painterResource(id = R.drawable.flame),
                             contentDescription = "flame",
@@ -135,61 +155,62 @@ fun HomeScreen(navController: NavHostController) {
                                 .clip(CircleShape)
                                 .size(16.dp)
                         )
-                        Spacer(modifier = Modifier.width(3.dp))
-
-                        Text(text = "348 kcal", color = Color.White, fontSize = 15.sp)
+                        Text(text = "${caloriesInfo.runCal} kcal", fontSize = 14.sp, color = Color.White, fontWeight = FontWeight.Bold)
                     }
                 }
             }
         }
 
-        Spacer(modifier = Modifier.height(15.dp))
+        Spacer(modifier = Modifier.height(16.dp))
 
         Box(
             modifier = Modifier
                 .fillMaxWidth()
+                .height(76.dp)
                 .border(2.dp, Color.White.copy(alpha = 0.3f), shape = RoundedCornerShape(48.dp))
-                .height(100.dp)
                 .clickable {
                     // navigate exercise
                 }
-
-        ) {
+        )
+        {
             Row(
                 modifier = Modifier
-                    .padding(20.dp)
+                    .padding(16.dp)
                     .fillMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.cycling),
-                    contentDescription = "running",
+                    contentDescription = "Cycling",
                     modifier = Modifier.clip(CircleShape)
                 )
-                Spacer(modifier = Modifier.width(15.dp))
 
-                Column() {
-                    Text(
-                        text = "Cycling",
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.White
-                    )
-                    Spacer(modifier = Modifier.height(5.dp))
-                    Text(text = "24 min", fontSize = 18.sp, color = Color.White)
+                Spacer(modifier = Modifier.width(12.dp))
+
+                Column()
+                {
+                    Text(text = "Cycling", fontSize = 18.sp, color = Color.White, fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = "$minutesCycle min", fontSize = 14.sp, color = Color.White, fontWeight = FontWeight.Bold)
                 }
 
-                Spacer(modifier = Modifier.width(50.dp))
+                Spacer(modifier = Modifier.width(56.dp))
 
-                Column() {
-                    Text(
-                        text = "4.22 km",
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.White
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalAlignment = Alignment.End
+                )
+                {
+                    Text(text = "$kilometersCycle km", fontSize = 18.sp, color = Color.White, fontWeight = FontWeight.Bold)
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Row(
+                        modifier = Modifier,
+                        verticalAlignment = Alignment.CenterVertically
                     )
-                    Spacer(modifier = Modifier.height(5.dp))
-                    Row {
+                    {
                         Image(
                             painter = painterResource(id = R.drawable.flame),
                             contentDescription = "flame",
@@ -197,61 +218,63 @@ fun HomeScreen(navController: NavHostController) {
                                 .clip(CircleShape)
                                 .size(16.dp)
                         )
-                        Spacer(modifier = Modifier.width(3.dp))
-
-                        Text(text = "248 kcal", color = Color.White, fontSize = 15.sp)
+                        Text(text = "${caloriesInfo.cycleCal} kcal", fontSize = 14.sp, color = Color.White, fontWeight = FontWeight.Bold)
                     }
                 }
             }
         }
 
-        Spacer(modifier = Modifier.height(15.dp))
+        Spacer(modifier = Modifier.height(16.dp))
 
         Box(
             modifier = Modifier
                 .fillMaxWidth()
+                .height(76.dp)
                 .border(2.dp, Color.White.copy(alpha = 0.3f), shape = RoundedCornerShape(48.dp))
-                .height(100.dp)
                 .clickable {
                     // navigate exercise
                 }
-
-        ) {
+        )
+        {
             Row(
                 modifier = Modifier
-                    .padding(20.dp)
+                    .padding(16.dp)
                     .fillMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.hiking),
-                    contentDescription = "hiking",
+                    contentDescription = "Hiking",
                     modifier = Modifier.clip(CircleShape)
+                        .size(width= 40.dp, height = 95.dp)
                 )
-                Spacer(modifier = Modifier.width(15.dp))
 
-                Column() {
-                    Text(
-                        text = "Hiking",
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.White
-                    )
-                    Spacer(modifier = Modifier.height(5.dp))
-                    Text(text = "24 min", fontSize = 18.sp, color = Color.White)
+                Spacer(modifier = Modifier.width(12.dp))
+
+                Column()
+                {
+                    Text(text = "Hiking", fontSize = 18.sp, color = Color.White, fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = "$minutesHike min", fontSize = 14.sp, color = Color.White, fontWeight = FontWeight.Bold)
                 }
 
-                Spacer(modifier = Modifier.width(50.dp))
+                Spacer(modifier = Modifier.width(56.dp))
 
-                Column() {
-                    Text(
-                        text = "5.56 km",
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.White
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalAlignment = Alignment.End
+                )
+                {
+                    Text(text = "$kilometersHike km", fontSize = 18.sp, color = Color.White, fontWeight = FontWeight.Bold)
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Row(
+                        modifier = Modifier,
+                        verticalAlignment = Alignment.CenterVertically
                     )
-                    Spacer(modifier = Modifier.height(5.dp))
-                    Row {
+                    {
                         Image(
                             painter = painterResource(id = R.drawable.flame),
                             contentDescription = "flame",
@@ -259,19 +282,19 @@ fun HomeScreen(navController: NavHostController) {
                                 .clip(CircleShape)
                                 .size(16.dp)
                         )
-                        Spacer(modifier = Modifier.width(3.dp))
-
-                        Text(text = "348 kcal", color = Color.White, fontSize = 15.sp)
+                        Text(text = "${caloriesInfo.hikeCal} kcal", fontSize = 14.sp, color = Color.White, fontWeight = FontWeight.Bold)
                     }
                 }
             }
         }
+
         Spacer(modifier = Modifier.height(30.dp))
 
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(20.dp)
+                .padding(20.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
         ) {
             Box(
                 modifier = Modifier
@@ -312,7 +335,8 @@ fun HomeScreen(navController: NavHostController) {
                         modifier = Modifier.padding(start = 40.dp),
                         text = "—",
                         fontSize = 50.sp,
-                        fontWeight = FontWeight.Bold
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
                     )
                 }
             }
@@ -356,7 +380,7 @@ fun HomeScreen(navController: NavHostController) {
 
                     Row(Modifier.padding(start = 30.dp)) {
                         Text(
-                            text = "24",
+                            text = "$minutesTotal",
                             color = Color.White,
                             fontWeight = FontWeight.Bold,
                             fontSize = 20.sp
@@ -367,6 +391,7 @@ fun HomeScreen(navController: NavHostController) {
                         Text(
                             text = "min",
                             color = Color.White,
+                            fontWeight = FontWeight.Bold,
                             fontSize = 15.sp
                         )
 
@@ -380,7 +405,8 @@ fun HomeScreen(navController: NavHostController) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(20.dp)
+                .padding(20.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
         ) {
             Box(
                 modifier = Modifier
@@ -419,7 +445,7 @@ fun HomeScreen(navController: NavHostController) {
 
                     Row(Modifier.padding(start = 30.dp)) {
                         Text(
-                            text = "10",
+                            text = "$kilometersTotal",
                             color = Color.White,
                             fontWeight = FontWeight.Bold,
                             fontSize = 20.sp
@@ -430,6 +456,7 @@ fun HomeScreen(navController: NavHostController) {
                         Text(
                             text = "km",
                             color = Color.White,
+                            fontWeight = FontWeight.Bold,
                             fontSize = 15.sp
                         )
 
@@ -476,7 +503,7 @@ fun HomeScreen(navController: NavHostController) {
 
                     Row(Modifier.padding(start = 30.dp)) {
                         Text(
-                            text = "42",
+                            text = "${caloriesInfo.totalCalories}",
                             color = Color.White,
                             fontWeight = FontWeight.Bold,
                             fontSize = 20.sp
@@ -487,6 +514,7 @@ fun HomeScreen(navController: NavHostController) {
                         Text(
                             text = "kcal",
                             color = Color.White,
+                            fontWeight = FontWeight.Bold,
                             fontSize = 15.sp
                         )
 
